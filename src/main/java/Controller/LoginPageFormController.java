17a1b7a098f5dd7099766151bd20bc4353c0fafa
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-18 18:32:47 +0530

    add employee login ui

--- src/main/java/Controller/LoginPageFormController.java
+++ src/main/java/Controller/LoginPageFormController.java
@@ -1,15 +1,21 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPageFormController implements Initializable {
+    Stage stage=new Stage();
     @FXML
     private MediaView mediaView;
 
@@ -34,5 +40,13 @@ public class LoginPageFormController implements Initializable {
         mediaView.setPreserveRatio(true);
 
 
+    }
+
+    public void loginBtnAdmin(ActionEvent actionEvent) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Admin/LoginPage.fxml"))));
+        stage.show();
+    }
+
+    public void loginBtn(ActionEvent actionEvent) {
     }
 }
