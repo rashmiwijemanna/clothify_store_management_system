f0e71d65e8d638d339e5eed3717cd9c2d3e65653
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-18 18:50:09 +0530

    add admin sidebar ui

---
+++ src/main/java/Controller/admin/SidebarFormController.java
@@ -0,0 +1,37 @@
+package Controller.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class SidebarFormController {
+
+    @FXML
+    private AnchorPane loadUi;
+
+    @FXML
+    void customerManagementBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void employeeManagementBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itemManagementBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportViewBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void suplierManagementBtn(ActionEvent event) {
+
+    }
+
+}
