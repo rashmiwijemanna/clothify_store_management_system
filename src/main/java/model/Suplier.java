81c7bc65dc4554e55edf1e697369158b2ecabb41
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-19 20:42:30 +0530

    add feilds to supplier model class

---
+++ src/main/java/model/Suplier.java
@@ -0,0 +1,17 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Suplier {
+    private String id;
+    private String name;
+    private String title;
+    private String email;
+    private String company;
+    private String phoneNumber;
+}
