d1d74acf27f40d72196c89293a9fc67aab36ce23
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-19 10:57:26 +0530

    employee ui implemented

---
+++ src/main/java/Controller/admin/EmployeeManagementFormController.java
@@ -0,0 +1,73 @@
+package Controller.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class EmployeeManagementFormController {
+
+    @FXML
+    private TableColumn<?, ?> addressCol;
+
+    @FXML
+    private Label dateLbl;
+
+    @FXML
+    private TableColumn<?, ?> emailCol;
+
+    @FXML
+    private TextField empAddressTxt;
+
+    @FXML
+    private TextField empEmailTxt;
+
+    @FXML
+    private TextField empIdTxt;
+
+    @FXML
+    private TextField empNameTxt;
+
+    @FXML
+    private TextField empPasswordTxt;
+
+    @FXML
+    private TextField empPhoneNumberTxt;
+
+    @FXML
+    private TableColumn<?, ?> idCol;
+
+    @FXML
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private TableColumn<?, ?> passwordCol;
+
+    @FXML
+    private TableColumn<?, ?> phoneNumberCol;
+
+    @FXML
+    private Label timeLbl;
+
+    @FXML
+    void addBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateBtn(ActionEvent event) {
+
+    }
+
+}
