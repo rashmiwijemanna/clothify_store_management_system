278028677e886f85c7bd9e5e7f6a97a25560df19
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-21 15:13:10 +0530

    add customer form in employee implemented

---
+++ src/main/java/Controller/employee/AddCustomerFormController.java
@@ -0,0 +1,52 @@
+package Controller.employee;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddCustomerFormController {
+
+    @FXML
+    private TextField custAddressTxt;
+
+    @FXML
+    private TextField custCity;
+
+    @FXML
+    private DatePicker custDOBTxt;
+
+    @FXML
+    private TextField custEmailTxt;
+
+    @FXML
+    private TextField custIdTxt;
+
+    @FXML
+    private TextField custNameTxt;
+
+    @FXML
+    private TextField custPhoneNumberTxt;
+
+    @FXML
+    private ComboBox<?> custTitleTxt;
+
+    @FXML
+    private Label dateLbl;
+
+    @FXML
+    private Label timeLbl;
+
+    @FXML
+    void addBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBtn(ActionEvent event) {
+
+    }
+
+}
